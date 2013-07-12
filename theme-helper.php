ad30eb8b137a38f0fc351b8f422e1f7d9829ab1d
melissacabral <melissacabral@gmail.com>
2013-07-12 08:13:39 -0700

    Update theme-helper.php

--- theme-helper.php
+++ theme-helper.php
@@ -97,7 +97,7 @@ function rad_help_head() {
 
 function adminhelper_currenturl() {
 	$pageURL = 'http';
-	if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	if ( isset( $_SERVER["HTTPS"] ) && strtolower( $_SERVER["HTTPS"] ) == "on" ) {$pageURL .= "s";}
 	$pageURL .= "://";
 	if ($_SERVER["SERVER_PORT"] != "80") {
 		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
